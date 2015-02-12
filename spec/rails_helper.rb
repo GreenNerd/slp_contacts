7d29765acfc3a253d5229c4d14f130b1a7f13eed
fahchen <dev.fah@gmail.com>
2015-02-12 20:36:37 +0800

    Add user model to dummy_app

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,11 @@ require 'spec_helper'
 require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
 
+Fabrication.configure do |config|
+  config.fabricator_path = '../fabricators'
+  config.sequence_start = 10000
+end
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
