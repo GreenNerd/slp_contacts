c061f8ce6ac6b29a6720d701b4014addc56e14ec
fahchen <dev.fah@gmail.com>
2015-02-13 14:12:42 +0800

    Add tag model to dummy_app

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150212171947) do
+ActiveRecord::Schema.define(version: 20150213061048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 20150212171947) do
   end
 
   add_index "organizations", ["parent_id"], name: "index_organizations_on_parent_id", using: :btree
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "user_organizations", force: :cascade do |t|
     t.integer  "user_id"
