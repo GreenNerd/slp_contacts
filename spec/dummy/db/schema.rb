915869e444ac025acccaef2de49b1533c94d4744
fahchen <dev.fah@gmail.com>
2015-03-03 08:55:09 +0800

    fix typo

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(version: 20150302035752) do
   add_index "user_organizations", ["organization_id"], name: "index_user_organizations_on_organization_id", using: :btree
   add_index "user_organizations", ["user_id"], name: "index_user_organizations_on_user_id", using: :btree
 
-  create_table "jsers", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "phone"
     t.string   "identifier"
