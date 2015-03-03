05dd1634fb4e0c0e39711f76027a3c7c915eb87b
fahchen <dev.fah@gmail.com>
2015-03-03 08:59:09 +0800

    add headimgurl to users

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150302035752) do
+ActiveRecord::Schema.define(version: 20150303005554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20150302035752) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.string   "remember_token"
+    t.string   "headimgurl"
   end
 
   add_foreign_key "slp_contacts_favorites", "users"
