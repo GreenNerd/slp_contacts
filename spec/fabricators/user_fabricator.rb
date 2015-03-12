cb31064779a5cd21dde513d6fb15ba2ad8a12711
fahchen <dev.fah@gmail.com>
2015-03-12 10:26:49 +0800

    Set the class_name of user fabricator
    [Issue #15] https://github.com/fahchen/slp_contacts/issues/15

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,4 +1,4 @@
-Fabricator(:user, aliases: [:contact]) do
+Fabricator(:user, class_name: SlpContacts.contact_class.to_s, aliases: [:contact]) do
   name { sequence(:name) { |i| "Name-#{i}"  }  }
   phone { sequence(:phone) { |i| 18683255107 + i }  }
   identifier { sequence(:identifier) { |i| rand(999_999_999) + i } }
