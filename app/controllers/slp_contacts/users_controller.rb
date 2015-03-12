d6e2d57255963a0021a4809b6cf22be9294ac6b5
huangwei <676018683@qq.com>
2015-03-12 12:03:05 +0800

    refactor (un)favorite one contact
    [Issue #14] https://github.com/fahchen/slp_contacts/issues/14

--- app/controllers/slp_contacts/users_controller.rb
+++ app/controllers/slp_contacts/users_controller.rb
@@ -8,17 +8,14 @@ module SlpContacts
     end
 
     def favorite
-      Favorite.create(user_id: current_user.id, contact_id: @user.id)
+      current_user.favorite(@user)
       respond_to do |f|
         f.js { render layout: false }
       end
     end
 
     def unfavorite
-      favorite = Favorite.find_by(user_id: current_user.id, contact_id: @user.id)
-      if favorite
-        favorite.destroy
-      else
+      unless current_user.unfavorite(@user)
         render js: "alert('没有收藏该联系人！');"
         return
       end
