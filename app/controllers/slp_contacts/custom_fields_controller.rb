4d7d35ed18f7b5994e15583f42c2a01abadea32b
huangwei <676018683@qq.com>
2015-03-17 16:14:16 +0800

    add custom_fields#create
    [Issue #31] https://github.com/fahchen/slp_contacts/issues/31

--- app/controllers/slp_contacts/custom_fields_controller.rb
+++ app/controllers/slp_contacts/custom_fields_controller.rb
@@ -2,9 +2,30 @@ require_dependency "slp_contacts/application_controller"
 
 module SlpContacts
   class CustomFieldsController < ApplicationController
+    before_action :find_namespace
+
     def new
-      @namespace = SlpContacts.namespace_class.first
       @custom_field = CustomField.new
+    end
+
+    def create
+      @custom_field = CustomField.new(custom_field_params)
+      @custom_field.namespace = @namespace
+      if @custom_field.save
+        render text: "success"
+      else
+        render text: "no success"
+      end
+    end
+
+    private
+
+    def find_namespace
+      @namespace = SlpContacts.namespace_class.first
+    end
+
+    def custom_field_params
+      params.require(:custom_field).permit(:name, :field_type, :is_required, :is_unique)
     end
   end
 end
--- spec/controllers/slp_contacts/custom_fields_controller_spec.rb
+++ spec/controllers/slp_contacts/custom_fields_controller_spec.rb
@@ -6,11 +6,28 @@ module SlpContacts
     let(:user) { Fabricate(:user) }
     let(:valid_session) { { current_user_id: user.id } }
 
+    before :each do
+      namespace = Fabricate(:namespace)
+    end
+
     describe "GET #new" do
       it "assigns a new CustomField to @custom_field" do
-        namespace = Fabricate(:namespace)
         get :new, {}, valid_session
         expect(assigns(:custom_field)).to be_a_new(CustomField)
+      end
+    end
+
+    describe "POST #create" do
+      it "saves the new custom_field in database" do
+        expect{
+          post :create, { custom_field: Fabricate.attributes_for(:custom_field) }, valid_session
+        }.to change(CustomField, :count).by(1)
+      end
+
+      it "fails to save when it is invaild" do
+        expect{
+          post :create, { custom_field: Fabricate.attributes_for(:custom_field, name: "") }, valid_session
+        }.to change(CustomField, :count).by(0)
       end
     end
 
