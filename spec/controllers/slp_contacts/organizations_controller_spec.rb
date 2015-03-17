b488c281a69c3f1ce331235d92815048f873d74d
fahchen <dev.fah@gmail.com>
2015-03-17 11:02:36 +0800

    Apply the scoped_organizations

--- spec/controllers/slp_contacts/organizations_controller_spec.rb
+++ spec/controllers/slp_contacts/organizations_controller_spec.rb
@@ -2,35 +2,31 @@ require 'rails_helper'
 
 module SlpContacts
   RSpec.describe OrganizationsController, type: :controller do
-
-    let(:user) { Fabricate(:user) }
+    let(:namespace) { Fabricate :namespace }
+    let(:organization) { Fabricate :organization, namespace: namespace }
+    let(:user) { Fabricate(:user, namespace: namespace) }
+    let!(:contact) { Fabricate(:user, name: 'xx1', namespace: namespace).tap { |u| organization.members << u } }
     let(:valid_session) { { current_user_id: user.id } }
 
     describe "GET #show" do
+
       it "assigns the requested organization to @organization" do
-        organization = Fabricate(:organization)
         get :show, { id: organization }, valid_session
         expect(assigns(:organization)).to eq organization
       end
 
       it "returns a json when format is json" do
-        organization = Fabricate(:organization)
-        contact1 = Fabricate(:user, name: 'xx1')
-        UserOrganization.create(user_id: contact1.id, organization_id: organization.id)
-        get :show, { id: organization, format: :json }, valid_session
-        json = JSON.parse(response.body)
-        expect(json[0]['name']).to eq contact1.name
+        xhr :get, :show, { id: organization, format: :json }, valid_session
+        parsed_body = JSON.parse(response.body)
+        expect(parsed_body[0]['name']).to eq contact.name
       end
     end
 
     describe "GET #query" do
       it "returns a json when user exists " do
-        contact1 = Fabricate(:user, name: 'xx1')
-        orga1 = Fabricate(:organization)
-        UserOrganization.create(user_id: contact1.id, organization_id: orga1.id)
-        get :query, { id: orga1.id, name: "xx1", format: :json }, valid_session
-        json = JSON.parse(response.body)
-        expect(json['results'][0]['name']).to eq contact1.name
+        xhr :get, :query, { id: organization.id, name: "xx1", format: :json }, valid_session
+        parsed_body = JSON.parse(response.body)
+        expect(parsed_body['results'][0]['name']).to eq contact.name
       end
     end
 
