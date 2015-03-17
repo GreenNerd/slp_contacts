650ba2f1126953dbed66f73f6bf2de7901ccdaf2
huangwei <676018683@qq.com>
2015-03-17 12:25:19 +0800

    add validations for custom_field model
    [Issue #31] https://github.com/fahchen/slp_contacts/issues/31

--- app/models/slp_contacts/custom_field.rb
+++ app/models/slp_contacts/custom_field.rb
@@ -2,5 +2,8 @@ module SlpContacts
   class CustomField < ActiveRecord::Base
     belongs_to :namespace, class_name: SlpContacts.namespace_class.to_s
     has_many :custom_values
+
+    validates :name, presence: true
+    validates :name, uniqueness: true
   end
 end
