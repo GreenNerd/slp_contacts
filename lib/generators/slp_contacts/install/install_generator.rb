ba8e2bc074c19a9656044e688698dd35d1edf3d5
fahchen <dev.fah@gmail.com>
2015-03-17 22:45:33 +0800

    Add slp_contacts generator

---
+++ lib/generators/slp_contacts/install/install_generator.rb
@@ -0,0 +1,33 @@
+require 'rails/generators/active_record'
+
+module SlpContacts
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      include Rails::Generators::Migration
+      attr_accessor :contact_class_name, :contact_table_name
+
+      desc 'Setup contacts app.'
+
+      source_root File.expand_path('../templates', __FILE__)
+
+      def self.next_migration_number(path)
+        Time.now.utc.strftime("%Y%m%d%H%M%S")
+      end
+
+      def set_contact_class_name
+        @contact_class_name = ask('The class_name of contact?[User]')
+        @contact_class_name = 'User' if @contact_class_name.blank?
+        @contact_class_name = @contact_class_name.camelize
+        @contact_table_name = @contact_class_name.underscore.pluralize
+      end
+
+      def copy_migration_files
+        migration_template 'migrate.rb', 'db/migrate/create_slp_contacts_favorites.rb'
+      end
+
+      def copy_config_files
+        template 'slp_contacts.rb', 'config/initializers/slp_contacts.rb'
+      end
+    end
+  end
+end
