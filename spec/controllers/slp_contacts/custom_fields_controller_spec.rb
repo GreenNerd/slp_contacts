40d18566181ed06eb1b1f471527005c63cf5c712
huangwei <676018683@qq.com>
2015-03-19 17:04:11 +0800

    Refactor custom field controller test
    [Issue #31] https://github.com/fahchen/slp_contacts/issues/31

--- spec/controllers/slp_contacts/custom_fields_controller_spec.rb
+++ spec/controllers/slp_contacts/custom_fields_controller_spec.rb
@@ -3,12 +3,11 @@ require 'rails_helper'
 module SlpContacts
   RSpec.describe CustomFieldsController, type: :controller do
 
-    let(:user) { Fabricate(:user) }
+    let(:namespace) { Fabricate(:namespace) }
+    let(:user) { Fabricate(:user, namespace: namespace) }
     let(:valid_session) { { current_user_id: user.id } }
-
-    before :each do
-      @namespace = Fabricate(:namespace)
-    end
+    let!(:custom_field) { Fabricate(:custom_field, namespace: namespace) }
+    let!(:custom_field1) { Fabricate(:custom_field, namespace: namespace) }
 
     describe "GET #new" do
       it "assigns a new CustomField to @custom_field" do
@@ -20,35 +19,31 @@ module SlpContacts
     describe "POST #create" do
       it "saves the new custom_field in database" do
         expect{
-          post :create, { custom_field: Fabricate.attributes_for(:custom_field) }, valid_session
+          post :create, { custom_field: Fabricate.attributes_for(:custom_field, namespace: namespace) }, valid_session
         }.to change(CustomField, :count).by(1)
       end
 
       it "fails to save when it is invaild" do
         expect{
-          post :create, { custom_field: Fabricate.attributes_for(:custom_field, name: "") }, valid_session
+          post :create, { custom_field: Fabricate.attributes_for(:custom_field, name: "", namespace: namespace) }, valid_session
         }.to change(CustomField, :count).by(0)
       end
     end
 
     describe "GET #index" do
       it "assigns the request custom_fields to @custom_fields" do
-        custom_field1 = Fabricate(:custom_field, namespace: @namespace)
-        custom_field2 = Fabricate(:custom_field, namespace: @namespace)
         get :index, {}, valid_session
-        expect(assigns(:custom_fields)).to match_array([custom_field1, custom_field2])
+        expect(assigns(:custom_fields)).to match_array([custom_field, custom_field1])
       end
     end
 
     describe "PUT #update" do
       it "returns code 200 when succeeding" do
-        custom_field = Fabricate(:custom_field)
         put :update, { id: custom_field, custom_field: Fabricate.attributes_for(:custom_field) }, valid_session
         expect(response).to have_http_status(200)
       end
 
       it "returns code 422 when failing to validate" do
-        custom_field = Fabricate(:custom_field)
         put :update, { id: custom_field, custom_field: Fabricate.attributes_for(:custom_field, name: "") }, valid_session
         expect(response).to have_http_status(422)
       end
@@ -56,7 +51,6 @@ module SlpContacts
 
     describe "DELETE #destroy" do
       it "returns code 200 and a json" do
-        custom_field = Fabricate(:custom_field)
         delete :destroy, { id: custom_field }, valid_session
         expect(response).to have_http_status(200)
       end
--- spec/fabricators/slp_contacts_custom_field_fabricator.rb
+++ spec/fabricators/slp_contacts_custom_field_fabricator.rb
@@ -1,4 +1,5 @@
 Fabricator(:custom_field, class_name: 'SlpContacts::CustomField') do
+  namespace
   name            { sequence(:name) { |i| "Name-#{i}"  }  }
   field_type      "输入框"
   possible_values "MyString"
