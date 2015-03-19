d66f2e97310278a66e406793ef5355f89febf6af
huangwei <676018683@qq.com>
2015-03-19 09:07:50 +0800

    add type param to remove column
    [Issue #31] https://github.com/fahchen/slp_contacts/issues/31

--- db/migrate/20150317031238_remove_type_from_custom_fields.rb
+++ db/migrate/20150317031238_remove_type_from_custom_fields.rb
@@ -1,5 +1,5 @@
 class RemoveTypeFromCustomFields < ActiveRecord::Migration
   def change
-    remove_column :slp_contacts_custom_fields, :type
+    remove_column :slp_contacts_custom_fields, :type, :string
   end
 end
--- db/migrate/20150317083015_change_field_type_in_custom_fields.rb
+++ db/migrate/20150317083015_change_field_type_in_custom_fields.rb
@@ -1,6 +1,6 @@
 class ChangeFieldTypeInCustomFields < ActiveRecord::Migration
   def change
-    remove_column :slp_contacts_custom_fields, :field_type
+    remove_column :slp_contacts_custom_fields, :field_type, :string
     add_column :slp_contacts_custom_fields, :field_type, :integer, default: 0
   end
 end
