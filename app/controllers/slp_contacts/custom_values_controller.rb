b8a12e22a2974e4bdf77812801b40222d6ecbbc0
huangwei <676018683@qq.com>
2015-03-20 11:29:43 +0800

    add custom_values#create
    [Issue #31] https://github.com/fahchen/slp_contacts/issues/31

--- app/controllers/slp_contacts/custom_values_controller.rb
+++ app/controllers/slp_contacts/custom_values_controller.rb
@@ -5,5 +5,27 @@ module SlpContacts
 
     def new
     end
+
+    def create
+      values_collection = []
+      current_user.namespace.custom_fields.each do |field|
+        if params[field.name].class == Array
+          @custom_value = current_user.custom_values.new(value: params[field.name].join(','), custom_field: field)
+        else
+          @custom_value = current_user.custom_values.new(value: params[field.name], custom_field: field)
+        end
+        if @custom_value.valid?
+          values_collection << @custom_value
+        else
+          render text: 'no success', status: 422
+          return
+        end
+      end
+      values_collection.each do |value|
+        value.save
+      end
+      render text: 'success'
+    end
+
   end
 end
--- spec/controllers/slp_contacts/custom_values_controller_spec.rb
+++ spec/controllers/slp_contacts/custom_values_controller_spec.rb
@@ -4,12 +4,29 @@ module SlpContacts
   RSpec.describe CustomValuesController, type: :controller do
     let(:user) { Fabricate :user }
     let(:valid_session) { { current_user_id: user.id } }
+    let(:custom_field) { Fabricate :custom_field, namespace: user.namespace }
+    let(:custom_field1) { Fabricate :custom_field, namespace: user.namespace }
 
     describe 'GET #new' do
-      it 'render the :new template' do
+      it 'renders the :new template' do
         get :new, {}, valid_session
         expect(response).to render_template :new
       end
     end
+
+    describe 'POSt #create' do
+      it 'adds CustomValue count by 2 when valid' do
+        expect{
+          post :create, { custom_field.name => 'value1', custom_field1.name => 'value2' }, valid_session
+        }.to change { CustomValue.count }.by(2)
+      end
+
+      it 'doesnot add CustomValue count when one is invalid' do
+        expect{
+          post :create, { custom_field.name => 'value1', custom_field1.name => nil }, valid_session
+        }.to change { CustomValue.count }.by(0)
+      end
+    end
+
   end
 end
