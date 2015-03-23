6bd0fa48ef34d36733d89c5628a8b9f6a2864a8a
huangwei <676018683@qq.com>
2015-03-23 11:35:46 +0800

    Refactor custom_values#create
    [Issue #31]

--- app/controllers/slp_contacts/custom_values_controller.rb
+++ app/controllers/slp_contacts/custom_values_controller.rb
@@ -7,24 +7,11 @@ module SlpContacts
     end
 
     def create
-      values_collection = []
-      current_user.namespace.custom_fields.each do |field|
-        if params[field.name].class == Array
-          @custom_value = current_user.custom_values.new(value: params[field.name].join(','), custom_field: field)
-        else
-          @custom_value = current_user.custom_values.new(value: params[field.name], custom_field: field)
-        end
-        if @custom_value.valid?
-          values_collection << @custom_value
-        else
-          render text: @custom_value.errors.full_messages, status: 422
-          return
-        end
+      if CustomValue.save_collection(current_user, params)
+        render text: 'success'
+      else
+        render text: 'failure', status: 422
       end
-      values_collection.each do |value|
-        value.save
-      end
-      render text: 'success'
     end
 
     def edit
