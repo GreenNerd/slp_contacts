d6de026a83dc37e7f86895efe29c94ce7efe40fb
Phil Chen <06fahchen@gmail.com>
2016-11-10 17:00:38 +0800

    Remove redundant step which setting default value

--- lib/generators/slp_contacts/install/templates/add_contact_public_to_users.rb
+++ lib/generators/slp_contacts/install/templates/add_contact_public_to_users.rb
@@ -1,7 +1,5 @@
 class AddContactPublicToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :<%= contact_table_name %>, :contact_public, :boolean, default: true
-
-    <%= contact_class_name %>.all.update_all contact_public: true
   end
 end
